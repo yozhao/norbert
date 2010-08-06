8c9a78e5eaebae8730ca8a0fd57ddfebd4c6ba78
Joshua Hartman <jhartman@linkedin.com>
2010-08-06 14:37:52 -0700

    Use sbt to publish src and javadoc jars, also upgrade sbt

--- project/build/NorbertProject.scala
+++ project/build/NorbertProject.scala
@@ -1,15 +1,24 @@
 import sbt._
-
 class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlugin {
   override def repositories = Set(ScalaToolsSnapshots, "JBoss Maven 2 Repository" at "http://repository.jboss.com/maven2")
 
   lazy val cluster = project("cluster", "Norbert Cluster", new ClusterProject(_))
   lazy val network = project("network", "Norbert Network", new NetworkProject(_), cluster)
-  lazy val javaCluster = project("java-cluster", "Norbert Java Cluster", new DefaultProject(_) with IdeaPlugin, cluster)
-  lazy val javaNetwork = project("java-network", "Norbert Java Network", new DefaultProject(_) with IdeaPlugin, javaCluster, network)
-  lazy val examples = project("examples", "Norbert Examples", new DefaultProject(_) with IdeaPlugin, network, javaNetwork)
+  lazy val javaCluster = project("java-cluster", "Norbert Java Cluster", new NorbertDefaults(_), cluster)
+  lazy val javaNetwork = project("java-network", "Norbert Java Network", new NorbertDefaults(_), javaCluster, network)
+  lazy val examples = project("examples", "Norbert Examples", new NorbertDefaults(_), network, javaNetwork)
 
-  class ClusterProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  class NorbertDefaults(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+    override def packageDocsJar = defaultJarPath("-javadoc.jar")
+    override def packageSrcJar = defaultJarPath("-sources.jar")
+    override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+
+    val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+    val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadocs"), Nil, None)
+
+  }
+
+  class ClusterProject(info: ProjectInfo) extends NorbertDefaults(info) {
     val zookeeper = "org.apache.hadoop" % "zookeeper" % "3.3.0" from "http://repo1.maven.org/maven2/org/apache/hadoop/zookeeper/3.3.0/zookeeper-3.3.0.jar"
     val protobuf = "com.google.protobuf" % "protobuf-java" % "2.1.0"
     val log4j = "log4j" % "log4j" % "1.2.14"
@@ -20,7 +29,7 @@ class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlu
     val objenesis = "org.objenesis" % "objenesis" % "1.0" % "test"
   }
 
-  class NetworkProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
+  class NetworkProject(info: ProjectInfo) extends NorbertDefaults(info) with IdeaPlugin {
     val netty = "org.jboss.netty" % "netty" % "3.1.0.GA"
     val slf4j = "org.slf4j" % "slf4j-api" % "1.5.6"
     val slf4jLog4j = "org.slf4j" % "slf4j-log4j12" % "1.5.6"
