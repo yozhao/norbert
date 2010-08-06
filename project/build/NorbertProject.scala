234e8b534cd4bab7890a032f135a88d0ae72120d
Joshua Hartman <jhartman@linkedin.com>
2010-08-06 13:46:23 -0700

    Downgrade protocol buffers and netty

--- project/build/NorbertProject.scala
+++ project/build/NorbertProject.scala
@@ -11,7 +11,7 @@ class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlu
 
   class ClusterProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
     val zookeeper = "org.apache.hadoop" % "zookeeper" % "3.3.0" from "http://repo1.maven.org/maven2/org/apache/hadoop/zookeeper/3.3.0/zookeeper-3.3.0.jar"
-    val protobuf = "com.google.protobuf" % "protobuf-java" % "2.3.0"
+    val protobuf = "com.google.protobuf" % "protobuf-java" % "2.1.0"
     val log4j = "log4j" % "log4j" % "1.2.14"
 
     val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test"
@@ -21,7 +21,7 @@ class NorbertProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlu
   }
 
   class NetworkProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin {
-    val netty = "org.jboss.netty" % "netty" % "3.1.5.GA"
+    val netty = "org.jboss.netty" % "netty" % "3.1.0.GA"
     val slf4j = "org.slf4j" % "slf4j-api" % "1.5.6"
     val slf4jLog4j = "org.slf4j" % "slf4j-log4j12" % "1.5.6"
   }
