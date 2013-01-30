fef7a1af38bc126420eb8764debc1e60dd2f4f5a
Yonghui Zhao <zhaoyonghui@gmail.com>
2013-01-30 18:13:42 +0800

    Implement nodesForPartitions

--- java-network/src/main/scala/com/linkedin/norbert/javacompat/network/JavaLbfToScalaLbf.scala
+++ java-network/src/main/scala/com/linkedin/norbert/javacompat/network/JavaLbfToScalaLbf.scala
@@ -2,11 +2,11 @@ package com.linkedin.norbert
 package javacompat
 package network
 
-import com.linkedin.norbert.network.partitioned.loadbalancer.{PartitionedLoadBalancerFactory => SPartitionedLoadBalancerFactory, PartitionedLoadBalancer => SPartitionedLoadBalancer}
-import com.linkedin.norbert.network.client.loadbalancer.{LoadBalancerFactory => SLoadBalancerFactory, LoadBalancer => SLoadBalancer}
+import com.linkedin.norbert.network.partitioned.loadbalancer.{ PartitionedLoadBalancerFactory => SPartitionedLoadBalancerFactory, PartitionedLoadBalancer => SPartitionedLoadBalancer }
+import com.linkedin.norbert.network.client.loadbalancer.{ LoadBalancerFactory => SLoadBalancerFactory, LoadBalancer => SLoadBalancer }
 
-import com.linkedin.norbert.cluster.{Node => SNode}
-import com.linkedin.norbert.network.common.{Endpoint => SEndpoint}
+import com.linkedin.norbert.cluster.{ Node => SNode }
+import com.linkedin.norbert.network.common.{ Endpoint => SEndpoint }
 
 import com.linkedin.norbert.EndpointConversions._
 
@@ -23,10 +23,10 @@ class JavaLbfToScalaLbf[PartitionedId](javaLbf: PartitionedLoadBalancerFactory[P
         var sMap = Map.empty[com.linkedin.norbert.cluster.Node, Set[Int]]
 
         val entries = jMap.entrySet.iterator
-        while(entries.hasNext) {
+        while (entries.hasNext) {
           val entry = entries.next
           val node = javaNodeToScalaNode(entry.getKey)
-          val set = entry.getValue.foldLeft(Set.empty[Int]) { (s, elem) => s + elem.intValue}
+          val set = entry.getValue.foldLeft(Set.empty[Int]) { (s, elem) => s + elem.intValue }
 
           sMap += (node -> set)
         }
@@ -38,17 +38,20 @@ class JavaLbfToScalaLbf[PartitionedId](javaLbf: PartitionedLoadBalancerFactory[P
         var sMap = Map.empty[com.linkedin.norbert.cluster.Node, Set[Int]]
 
         val entries = jMap.entrySet.iterator
-        while(entries.hasNext) {
+        while (entries.hasNext) {
           val entry = entries.next
           val node = javaNodeToScalaNode(entry.getKey)
-          val set = entry.getValue.foldLeft(Set.empty[Int]) { (s, elem) => s + elem.intValue}
-
-          sMap += (node -> set)
+          val set = partitions.foldLeft(Set.empty[Int]) {
+            (s, elem) => if (entry.getValue.contains(elem)) s + elem.intValue else s
+          }
+          if (set.size > 0) {
+            sMap += (node -> set)
+          }
         }
         sMap
       }
     }
-    
+
   }
 
   def getNumPartitions(endpoints: Set[SEndpoint]) = javaLbf.getNumPartitions(endpoints).intValue()
